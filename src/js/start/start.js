b80df2f4f820488885ded43d69d4b137a836a778
Leander <nander@nander.dev>
2020-05-27 16:06:08 +0200

    changed filestructure and updated node-sass config

--- src/js/start/start.js
+++ src/js/start/start.js
@@ -2,10 +2,9 @@ const {ipcMain} = require('electron');
 const path = require('path');
 
 ipcMain.on('new', () => {
-  console.log('hi');
+  // TODO
 });
 ipcMain.on('load', () => {
-  console.log('hi');
 });
 ipcMain.on('settings', (event) => {
   event.sender.loadFile(path.join(__dirname, '..', 'settings.html'));
